9d298ae932a22b75ca7c8d30ea69a501158f7a05
Danny Cho <dannycho7@gmail.com>
2016-12-16 13:25:28 -0800

    Removed more unneccessary files

--- date.js
+++
@@ -1,5 +0,0 @@
-function updateTime(){
-document.getElementById('date').innerHTML = Date();
-}
-updateTime();
-setInterval(updateTime,1000);
--- loading.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo '<div id="loading-wrapper">
-	<div id="loader"><img src="pics/box.gif"></div>
-	<div class="loader-section section-left"></div>
-	<div class="loader-section section-right"></div>
-</div>';
-?>
