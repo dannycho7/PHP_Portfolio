65e26d65f51ec58bf1be46fa9b4ae2834feb7032
Danny H. Cho <dannycho7@gmail.com>
2017-02-18 22:15:34 -0800

    Update for ext

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ require_once 'modules/header.php'
 			<h1 id = 'projh1'>Project Overview</h1>
 			<?php include 'modules/projinfoXML.php';
 				foreach($projInfo->project as $proj){
-					echo "<div class='proj-container-container'><a href='projects#".$proj['name']."' class='content-proj-container'><p>".$proj['name']."</p><ul><li>".$proj['year']."</li><li>".$proj['accomplishment']."</li><li>".$proj['purpose']."</li></ul></a></div>";
+					echo "<div class='proj-container-container'><a href='projects#".$proj['name']."' class='content-proj-container'><p>".$proj['displayname']."</p><ul><li>".$proj['year']."</li><li>".$proj['accomplishment']."</li><li>".$proj['purpose']."</li></ul></a></div>";
 				}
 			?>
 		</div>
--- projects.php
+++ projects.php
@@ -41,7 +41,7 @@ require_once 'modules/landingpage.php';
                 <div class='proj-preview'>
                   <img src='pics/project/".$proj['name'].".png' alt='No picture available' />
                 </div>
-                <h1>".$proj['name']."</h1>
+                <h1>".$proj['displayname']."</h1>
                 <div class='description'>
                   <div class = 'block'>
                     <span>".$proj['purpose']."</span>
@@ -52,10 +52,15 @@ require_once 'modules/landingpage.php';
                   </div>
                   <div class = 'block'>
                     <span>".$proj['accomplishment']."</span>
-                  </div>
-                </div>
-                <article>".$str."</article>
+                  </div>";
+        if(isset($proj['link'])){
+        echo "<div>
+                <a target='_blank' href=".$proj['link']." class='readMore'><span>Download Link</span></a>
               </div>";
+        }
+        echo  "</div>
+            <article>".$str."</article>
+          </div>";
       }
     ?>
   </div><!--loaded-->
