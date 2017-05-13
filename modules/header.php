8d3c7a09ceb765352ce175e7f49da98484d49b99
dannycho7 <dannycho7@gmail.com>
2017-05-12 18:35:09 -0700

    remove resume on new tab

--- modules/header.php
+++ modules/header.php
@@ -6,8 +6,8 @@ echo '<div id="navigation" class="gradient">
         </a>
         </div>
       <a id="menu-icon" class="navselect" onclick="reveal()">
-      <img src="pics/menu-icon.png" id="menuimg"></img> 
-      <img src="pics/exit.png" id="exit"></img> 
+      <img src="pics/menu-icon.png" id="menuimg"></img>
+      <img src="pics/exit.png" id="exit"></img>
       </a>
       <div id="container">
         <div id="menubar">
@@ -15,7 +15,7 @@ echo '<div id="navigation" class="gradient">
         <li class="linknav"><a onclick="navigate(\'index\');" class="navselect" '.$homecolor.'>Home</a></li><!--
         --><li class="linknav"><a onclick="navigate(\'projects\');" class="navselect" '.$projectscolor.'>Projects</a></li><!--
         --><li class="linknav"><a onclick="navigate(\'photo\');" class="navselect" '.$photocolor.'>Photography</a></li><!---->
-        <li class="linknav"><a href="pics/Resume_DannyHyunCho.pdf" class="navselect" target="_blank">Resume</a></li>
+        <li class="linknav"><a href="pics/Resume_DannyHyunCho.pdf" class="navselect">Resume</a></li>
       </ul>
       </div><!--menubar-->
       </div><!--container-->
