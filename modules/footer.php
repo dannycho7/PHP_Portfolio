479e78df155cd30fe9216eed1b166e7e738f0ed7
dannycho7 <dannycho7@gmail.com>
2017-05-15 02:28:33 -0700

    footer partial in modules

---
+++ modules/footer.php
@@ -0,0 +1,7 @@
+</body>
+<script src = 'js/jquery-3.1.0.min.js'></script>
+<script src = 'js/web.js'></script>
+<script src = 'js/index.js'></script>
+<script src = 'js/photo.js'></script>
+<script type="text/javascript" src="js/projects.js"></script>
+</html>
