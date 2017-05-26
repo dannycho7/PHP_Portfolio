f42ec73cf805c161d6ec1254f25a0ee1ab527411
dannycho7 <dannycho7@gmail.com>
2017-05-26 00:04:35 -0700

    eslint linting

--- js/photo.js
+++ js/photo.js
@@ -3,26 +3,27 @@ $(document).ready(function(){
 });
 function iconresize(){
 	if(document.getElementById("photo-album-icon")){
-	var lineheight=parseInt($("p").css("line-height"));
-	document.getElementById("photo-album-icon").style.height=lineheight+"px";
+		var lineheight=parseInt($("p").css("line-height"));
+		document.getElementById("photo-album-icon").style.height=lineheight+"px";
+	}
 }
-}
-window.addEventListener("resize",function(){iconresize();},false);
-
+window.addEventListener("resize", function(){
+	iconresize();
+},false);
 
 function showPhoto(str){
 	var	xhttp = new XMLHttpRequest();
-	$('html, body').animate({
-        scrollTop: $("#content-container").offset().top - '80'
-    }, 400);
+	$("html, body").animate({
+		scrollTop: $("#content-container").offset().top - "80"
+	}, 400);
 	xhttp.onreadystatechange = function(){
 		if(this.readyState == 4 && this.status == 200){
 			document.body.innerHTML = this.responseText;
 			if(str != ""){
-				window.history.pushState({}, 'Danny\'s Personal Site', 'photo?folder='+str);
+				window.history.pushState({}, "Danny's Personal Site", "photo?folder="+str);
 			}
 			else{
-				window.history.pushState({}, 'Danny\'s Personal Site', 'photo');
+				window.history.pushState({}, "Danny's Personal Site", "photo");
 			}
 		}
 	};
--- js/web.js
+++ js/web.js
@@ -1,37 +1,34 @@
 var revealed = false;
 function reveal(){ //this method will change the class
-	var menu = document.getElementById('container');
+	var menu = document.getElementById("container");
 	revealed=true;
-	console.log("reveal");
-	if(getComputedStyle(menu).visibility=='hidden'){
-		menu.style.visibility='visible';
+	if(getComputedStyle(menu).visibility=="hidden"){
+		menu.style.visibility="visible";
 		menu.style.opacity=1;
-		document.getElementById('navigation').classList.toggle('gradientCntrl');
-		document.getElementById('menuimg').style.display='none';
-		document.getElementById('exit').style.display='inline-block';
+		document.getElementById("navigation").classList.toggle("gradientCntrl");
+		document.getElementById("menuimg").style.display="none";
+		document.getElementById("exit").style.display="inline-block";
 	}
 	else{
-		document.getElementById('navigation').classList.toggle('gradientCntrl');
-		menu.style.visibility='hidden';
+		document.getElementById("navigation").classList.toggle("gradientCntrl");
+		menu.style.visibility="hidden";
 		menu.style.opacity=0;
-		document.getElementById('menuimg').style.display='inline-block';
-		document.getElementById('exit').style.display='none';
+		document.getElementById("menuimg").style.display="inline-block";
+		document.getElementById("exit").style.display="none";
 	}
 }
 function menuswitch(){//runs when window gets resized
-	if(getComputedStyle(document.getElementById('menu-icon')).display=='none' && revealed){
-		console.log("yes");
-		var menu = document.getElementById('container');
-		menu.style.visibility='visible';
+	if(getComputedStyle(document.getElementById("menu-icon")).display=="none" && revealed){
+		var menu = document.getElementById("container");
+		menu.style.visibility="visible";
 		menu.style.opacity=1;
 		revealed=false;
 	}
-	if(getComputedStyle(document.getElementById('menuimg')).display=='inline-block' && getComputedStyle(document.getElementById('menu-icon')).display !='none'){
-		var menu = document.getElementById('container');
-		menu.style.visibility='hidden';
+	if(getComputedStyle(document.getElementById("menuimg")).display=="inline-block" && getComputedStyle(document.getElementById("menu-icon")).display !="none"){
+		var menu = document.getElementById("container");
+		menu.style.visibility="hidden";
 		menu.style.opacity=0;
 		revealed = false;
-		console.log("lol");
 	}
 }
 function navigate(str){
@@ -41,7 +38,7 @@ function navigate(str){
 	xhttp.onreadystatechange = function(){
 		if(xhttp.readyState == 4 && xhttp.status == 200){
 			document.body.innerHTML = xhttp.responseText;
-			window.history.pushState({}, 'Danny\'s Personal Site', str);
+			window.history.pushState({}, "Danny's Personal Site", str);
 		}
 	};
 	xhttp.open("GET", str, true);
@@ -55,7 +52,7 @@ function showInfo(element,projName){
 		if(xhttp.readyState == 4 && xhttp.status == 200){
 			content.innerHTML = xhttp.responseText;
 		}
-	}
+	};
 	xhttp.open("GET","modules/projDescr.php?name=" + projName,true);
 	xhttp.send();
 }
